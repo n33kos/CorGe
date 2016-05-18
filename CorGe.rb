682951a792f542ae5bd6d63a3232d942215a7236
Nicholas Suski <n33kos@gmail.com>
2016-05-17 22:57:06 -0600

    Basic replacement functionality

---
+++ CorGe.rb
@@ -0,0 +1,20 @@
+#------------------------------------------
+#---Correspondence Generator (CorGe) v0.0.1
+#------------------------------------------
+
+output_file = "sample.txt"
+template_file = "templates/template_test.rb"
+
+template = File.open(template_file, 'r')
+output = File.open(output_file, 'w')
+
+template.each_line do |line|
+	getval = {}
+	match = line.scan(/(\{.+?\})/)
+	match.each do |m|
+		puts m[0]
+		getval[m[0]] = gets.chomp
+	end
+	output.puts(line.gsub(/(\{.+?\})/, getval))
+	print line.gsub(/(\{.+?\})/, getval)
+end
---
+++ templates/template_test.rb
@@ -0,0 +1,4 @@
+Lorem ipsum dolor sit amet {First Name}.
+Ut convallis vitae libero justo daibus {Last Name}.
+Diam non, vestibulum gravida nulla nam {Email}.
+Accumsan lectus, nam litora et vivamus {Phone Number}.
