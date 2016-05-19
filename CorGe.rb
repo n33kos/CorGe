66b35636674d99341156d8447061c5fe0ae58c9c
Nicholas Suski <n33kos@gmail.com>
2016-05-18 22:22:19 -0600

    Expanded configs.

--- CorGe.rb
+++ CorGe.rb
@@ -1,35 +1,12 @@
 #------------------------------------------
-#---Correspondence Generator (CorGe) v0.0.1
+#---Correspondence Generator (CorGe) v0.0.2
 #------------------------------------------
 require 'yaml'
 require 'csv'
 require 'mail'
 
 #------------Functions----------
-=begin
-def test
-	output_file = "test.txt"
-	template_file = "templates/template_test.rb"
-
-	template = File.open(template_file, 'r')
-	output = File.open(output_file, 'w')
-
-	template.each_line do |line|
-		getval = {}
-		match = line.scan(/(\{.+?\})/)
-		match.each do |m|
-			puts m[0]
-			getval[m[0]] = gets.chomp
-		end
-		output.puts(line.gsub(/(\{.+?\})/, getval))
-		print line.gsub(/(\{.+?\})/, getval)
-	end
-	template.close
-	output.close
-end
-=end
-
-def read_config
+def read_configs
 	@config = YAML.load_file("config.yaml")
 end
 
@@ -46,7 +23,7 @@ def generate_from_csv output_path, csv_path, template_path
 			titles = row
 		else
 			template = File.open(template_path, 'r')
-			output = File.open(output_path+"correspondence_"+row[0]+"_"+Time.now.strftime('%Y-%m-%d_%H%M%S')+".corge", 'w')
+			output = File.open(output_path+@config[:generated_filename_prefix]+row[0]+"_"+Time.now.strftime('%Y-%m-%d_%H%M%S')+".corge", 'w')
 			template.each_line do |line|
 				replace = {}
 				row.each_with_index do |r, i|
@@ -58,7 +35,7 @@ def generate_from_csv output_path, csv_path, template_path
 				end
 				output.puts(line.gsub(/(\{.+?\})/, replace))
 			end
-			print "generated "+output_path+"correspondence_"+row[0]+"_"+Time.now.strftime('%Y-%m-%d_%H%M%S')+".corge"+".\n"
+			print "generated "+output_path+@config[:generated_filename_prefix]+row[0]+"_"+Time.now.strftime('%Y-%m-%d_%H%M%S')+".corge"+".\n"
 			output.close
 			template.close
 		end
@@ -73,12 +50,14 @@ def send_generated_messages custom_config
 		mail.subject = @config[:subject]
 		mail.body = File.read(correspondence)
 		mail.deliver!
+		File.rename(correspondence, correspondence+".sent")
 	end
 end
 
 #------------Init----------
-#test
-read_config
+read_configs
 email_options @config[:mail_options]
 generate_from_csv(@config[:generated],@config[:csv],@config[:template])
-send_generated_messages nil
+if @config[:send_mail]
+	send_generated_messages nil
+end
