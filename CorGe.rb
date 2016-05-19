041ca170fdf30596364758cfafdce92a9734f078
Nicholas Suski <n33kos@gmail.com>
2016-05-18 21:19:13 -0600

    Implemented mail gem, expanded configs

--- CorGe.rb
+++ CorGe.rb
@@ -1,6 +1,9 @@
 #------------------------------------------
 #---Correspondence Generator (CorGe) v0.0.1
 #------------------------------------------
+require 'yaml'
+require 'csv'
+require 'mail'
 
 #------------Functions----------
 =begin
@@ -27,19 +30,23 @@ end
 =end
 
 def read_config
-	require 'yaml'
 	@config = YAML.load_file("config.yaml")
 end
 
+def email_options mail_options
+	Mail.defaults do
+		delivery_method :smtp, mail_options
+	end
+end
+
 def generate_from_csv output_path, csv_path, template_path
-	require 'csv'
 	titles = []
 	CSV.foreach(csv_path) do |row|
 		if $. == 1
 			titles = row
 		else
 			template = File.open(template_path, 'r')
-			output = File.open(output_path+"correspondence_"+row[0]+"_"+Time.now.strftime('%Y-%m-%d_%H%M%S'), 'w')
+			output = File.open(output_path+"correspondence_"+row[0]+"_"+Time.now.strftime('%Y-%m-%d_%H%M%S')+".corge", 'w')
 			template.each_line do |line|
 				replace = {}
 				row.each_with_index do |r, i|
@@ -51,18 +58,27 @@ def generate_from_csv output_path, csv_path, template_path
 				end
 				output.puts(line.gsub(/(\{.+?\})/, replace))
 			end
-			print "generated "+output_path+"correspondence_"+row[0]+"_"+Time.now.strftime('%Y-%m-%d_%H%M%S')+".\n"
+			print "generated "+output_path+"correspondence_"+row[0]+"_"+Time.now.strftime('%Y-%m-%d_%H%M%S')+".corge"+".\n"
 			output.close
 			template.close
 		end
 	end
 end
 
-def send_generate_messages
+def send_generated_messages custom_config
+	Dir.glob(@config[:generated]+'*.corge') do |correspondence|
+		mail = Mail.new
+		mail.from = @config[:from_email]
+		mail.to = @config[:to_email]
+		mail.subject = @config[:subject]
+		mail.body = File.read(correspondence)
+		mail.deliver!
+	end
 end
 
 #------------Init----------
 #test
 read_config
-puts @config
-generate_from_csv('generated/',@config['csv'],@config['template'])
+email_options @config[:mail_options]
+generate_from_csv(@config[:generated],@config[:csv],@config[:template])
+send_generated_messages nil
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 CorGe (Correspondence Generator) is a configurable ruby template processor developed for automated correspondence.
 
 # Requirements
-- Ruby (Developed with 2.0.0)
+- Ruby v2.0.0
 - Gems:
 	- mail
